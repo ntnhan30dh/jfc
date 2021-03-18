4be43107b23c2f095e2c5cffd492683af1d8f6d9
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2021-03-18 16:17:03 +0100

    open link in new tab

--- src/components/countrylist.js
+++ src/components/countrylist.js
@@ -2,12 +2,12 @@ import React from "react"
 import { Dropdown } from 'semantic-ui-react'
 
 const countryOptions = [
-  { key: 'at', value: 'at', flag: 'at', text: 'Austria ' , as: 'a', href:'https://www.mjam.net/'},
-  { key: 'my', value: 'my', flag: 'my', text: 'Malaysia ', as: 'a', href:'https://www.foodpanda.my/ ' },
-  { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh', as: 'a', href:'https://www.foodpanda.com.bd/' },
-  { key: 'hk', value: 'hk', flag: 'hk', text: 'Hongkong', as: 'a', href:'https://www.foodpanda.hk/' },
-  { key: 'ph', value: 'ph', flag: 'ph', text: 'Philippines ', as: 'a', href:'https://www.foodpanda.ph/ ' },
-  { key: 'th', value: 'th', flag: 'th', text: 'Thailand ', as: 'a', href:'https://www.foodpanda.co.th/ '},
+  { key: 'at', value: 'at', flag: 'at', text: 'Austria ' , as: 'a', href:'https://www.mjam.net/',target:"_blank"},
+  { key: 'my', value: 'my', flag: 'my', text: 'Malaysia ', as: 'a', href:'https://www.foodpanda.my/', target:"_blank" },
+  { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh', as: 'a', href:'https://www.foodpanda.com.bd/', target:"_blank" },
+  { key: 'hk', value: 'hk', flag: 'hk', text: 'Hongkong', as: 'a', href:'https://www.foodpanda.hk/',target:"_blank"  },
+  { key: 'ph', value: 'ph', flag: 'ph', text: 'Philippines ', as: 'a', href:'https://www.foodpanda.ph/ ', target:"_blank" },
+  { key: 'th', value: 'th', flag: 'th', text: 'Thailand ', as: 'a', href:'https://www.foodpanda.co.th/ ', target:"_blank"},
  
 ]
 
