da37c477f978328de545a6572feeb9d9a80370e1
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2021-06-02 17:29:43 +0200

    soMe 9 pics

--- src/components/followus.js
+++ src/components/followus.js
@@ -7,8 +7,6 @@ import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
-
-
 const FollowUs = () => {
   const data = useStaticQuery(graphql`
     {
@@ -33,12 +31,129 @@ const FollowUs = () => {
           }
         }
       }
+
+      soMe4: file(relativePath: { eq: "soMe4.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 400) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      soMe5: file(relativePath: { eq: "soMe5.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      soMe6: file(relativePath: { eq: "soMe6.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
     }
   `)
   return (
     <section className="followUsContainer" id="follow">
+      <h2>
+        Feeling tender? Or longing for burgers? <br /> Whatever your chicken
+        choice, make it Jackson's.
+      </h2>
+      <div className="soMe-container">
+        <div className="left">
+          <span className="menu-sidebar">FOLLOW US</span>
+        </div>
+        <div className="main-soMe">
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe2.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe1.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe3.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe2.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe1.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe3.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe2.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe1.childImageSharp.fluid} />
+            </Link>
+          </div>
+          <div className="pic">
+            <Link
+              to="https://www.instagram.com/jacksons_fried_chicken/"
+              className=""
+              target="_blank"
+            >
+              <Img fluid={data.soMe3.childImageSharp.fluid} />
+            </Link>
+          </div>
+        </div>
+        <div className="right">
+          <span className="menu-sidebar">FOLLOW US</span>
+        </div>
+      </div>
       <div className="redDiv">
-      <div className="followUsText">
+        {/* <div className="followUsText">
           <h4>follow us</h4>
           <div className="icons">
             <div className="img_div">
@@ -50,8 +165,8 @@ const FollowUs = () => {
               <img src={facebook} alt="logo" />
             </div>
           </div>
-        </div>
-      <div className="soMe">
+        </div> */}
+        {/* <div className="soMe">
         <div className="pic">
           <Link to="https://www.instagram.com/jacksons_fried_chicken/" className="" target="_blank">
             <Img fluid={data.soMe2.childImageSharp.fluid} />
@@ -67,21 +182,21 @@ const FollowUs = () => {
             <Img fluid={data.soMe3.childImageSharp.fluid} />
           </Link>
         </div>
-      </div>
+      </div> */}
         <div className="top">
           <div className="right">
-            <h2>
+            {/* <h2>
             Feeling tender?<br/> Or longing for burgers? <br/>  Whatever your chicken <br/>
             choice, make it Jackson's.
-            </h2>
+            </h2> */}
           </div>
           <div className="left">
             <div className="img_div">
               <img src={logo} alt="logo" />
             </div>
           </div>
-        <div className="bottom">
-          {/* <h4>follow us</h4>
+          <div className="bottom">
+            {/* <h4>follow us</h4>
           <div className="icons">
             <div className="img_div">
             <Link to="https://www.instagram.com/jacksons_fried_chicken/" target="_blank">
@@ -93,18 +208,18 @@ const FollowUs = () => {
             </div>
           </div> */}
 
-          <ul>
-          <li>
-            <Link to="/imprint">Imprint</Link>
-            </li>
-            <li>
-            <Link to="/privacy">Privacy Policy </Link>
-            </li>
-            <li>
-            <Link to="/disclaimer">Disclaimer</Link>
-            </li>
-        </ul>
-        </div>
+            <ul>
+              <li>
+                <Link to="/imprint">Imprint</Link>
+              </li>
+              <li>
+                <Link to="/privacy">Privacy Policy </Link>
+              </li>
+              <li>
+                <Link to="/disclaimer">Disclaimer</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       {/* <div className="blackDiv">
